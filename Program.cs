e02ac10ae26942ef19113cf8c001ee8a09910214
lerakenchadze <119068926+lerakenchadze@users.noreply.github.com>
2022-12-11 22:02:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿// Console.WriteLine("How old are you?");
+// int test = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("After 6 year you will be a"+(test+6)+" years old");
+
+
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+// Console.Write("Enter your number: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Square of number {numb} is {numb*numb});
+// Console.WriteLine("Square of number " +numb+ " is "+ (numb*numb)");
+
+// Напишите программу, которая на вход принимает два числа и проверяет является ли первое число квадратом второго.
+
+
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (b == a * a )
+//{
+ //   Console.WriteLine($"Число {a} является квадратом числа {b}");
+// }
+// else
+// {
+ //   Console.WriteLine("не является");
+// }
+
+
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+//Console.WriteLine("Введите номер дня недели");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if (num == 1)
+//{
+   // Console.WriteLine("Понедельник");
+//}
+//else if (num == 2)
+//{
+  //  Console.WriteLine("Вторник");
+//}
+//else if (num == 3)
+//{
+  //  Console.WriteLine("Среда");
+//}
+//else if (num == 4)
+//{
+  //  Console.WriteLine("Четверг");
+//}
+//else if (num == 5)
+//{
+  //  Console.WriteLine("Пятница");
+//}
+//else if (num == 6)
+//{
+  //  Console.WriteLine("Суббота");
+//}
+//else if (num == 7)
+//{
+  //  Console.WriteLine("Воскресенье");
+//}
+//else 
+  //  Console.WriteLine("Error!");
+
+  // Напишите программу которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int t = num* (-1);
+
+// /while (t < num-1)
+// {
+//   t +=1;
+//   Console.WriteLine();
+//   Console.Write(t + " ");
+// }
+
+  // Напишите программу, которая принимает на вход трёхзачное число и на выходе показывает последнюю цифру этого числа
+
+
+// Console.WriteLine("Введите трёхзначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n>99 & n<1000)
+// {
+//     Console.WriteLine(n%10);
+// }
+// else
+// {
+//     Console.WriteLine("Error");
+// }   
+
+
+
+
+
